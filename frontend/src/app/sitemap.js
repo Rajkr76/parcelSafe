f2ec8527610f893a08ae91622f316ba890356329
Raj kumar <186265760+Rajkr76@users.noreply.github.com>
2026-07-09 17:09:35 +0530

    fixing ome changes in sitemap js

--- frontend/src/app/sitemap.js
+++ frontend/src/app/sitemap.js
@@ -1,20 +1,20 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://parcelsafe.vercel.app',
+      url: 'https://vercel.app',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1.0,
     },
     
     {
-      url: 'https://parcelsafe.vercel.app/auth',
+      url: 'https://vercel.app/auth',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://parcelsafe.vercel.app/onboarding',
+      url: 'https://vercel.app/onboarding',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
